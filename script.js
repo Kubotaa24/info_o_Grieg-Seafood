b8aa9d4be96e674d32ea32e9b715d618d670e505
Kubotaa24 <d.jazdzewski877@gmail.com>
2024-09-17 07:43:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.createElement('button');
-    button.textContent = 'Kliknij mnie jezeli mnie Kochasz!';
+    button.textContent = 'Kliknij mnie jezeli mi nie wierzysz!';
     document.body.appendChild(button);
 
     button.addEventListener('click', () => {
-        alert('Twój Pawian też Cie Kocha!');
+        alert('https://www.nordnet.no/market/stocks/16105714-grieg-seafood');
     });
 });
